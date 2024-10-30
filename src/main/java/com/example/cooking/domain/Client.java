47f99c338dd45f4546182aeb0a4ab0a4fc187dfb
do3642 <61033883+do3642@users.noreply.github.com>
2024-10-30 23:27:22 +0900

    entity: client 생성 - 시퀀스,id,패스워드,닉네임,이메일,전화번호,게시글개수,권한,생성시간

---
+++ src/main/java/com/example/cooking/domain/Client.java
@@ -0,0 +1,52 @@
+package com.example.cooking.domain;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false, length = 40)
+    private String username;
+
+    @Column(nullable = false, length = 40)
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 40)
+    private String nickname;
+
+    @Column(unique = true, nullable = false, length = 40)
+    private String email;
+
+    @Column(nullable = false, length = 15) 
+    private String tel;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    // 게시글 개수에 따라 등급 상승을 위한 필드
+    private int postCount = 0; // 초기값 0
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role; 
+}
---
+++ src/main/java/com/example/cooking/domain/RoleType.java
@@ -0,0 +1,5 @@
+package com.example.cooking.domain;
+
+public enum RoleType {
+	USER, ADMIN;
+}
